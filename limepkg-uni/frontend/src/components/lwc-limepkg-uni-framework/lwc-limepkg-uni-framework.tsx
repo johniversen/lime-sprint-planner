cce8a2102f58e5fd7aec80614e5095a41e005df0
hanna <fellwinghanna@gmail.com>
2019-11-04 14:02:39 +0100

    Added filter and calender/week overview component. Fixed header and grid is now correct. Lime colors and icons not working atm, but changed to their hex colors

--- limepkg-uni/frontend/src/components/lwc-limepkg-uni-framework/lwc-limepkg-uni-framework.tsx
+++ limepkg-uni/frontend/src/components/lwc-limepkg-uni-framework/lwc-limepkg-uni-framework.tsx
@@ -2,16 +2,17 @@ import {
     LimeWebComponent,
     LimeWebComponentContext,
     LimeWebComponentPlatform,
-    //NotificationService,
-    //PlatformServiceName,
+
 } from '@limetech/lime-web-components-interfaces';
-import { Component, Element, h, Prop } from '@stencil/core';
+import { Component, Element, h, Prop, State } from '@stencil/core';
+import  { Option } from '@limetech/lime-elements';
 
 @Component({
     tag: 'lwc-limepkg-uni-framework',
     shadow: true,
     styleUrl: 'lwc-limepkg-uni-framework.css',
 })
+
 export class Framework implements LimeWebComponent {
     @Prop()
     public platform: LimeWebComponentPlatform;
@@ -22,19 +23,59 @@ export class Framework implements LimeWebComponent {
     @Element()
     public element: HTMLElement;
 
+    @State()
+     private dateValue = new Date();
+
+    @State()
+    public selectValue: Option;
+
+    @State()
+    private options: Option[] = [
+        { text: 'Deal', value: 'deal' },
+        { text: 'Solution Improvement', value: 'solutionimprovement'}
+    ]
+
+    constructor() {
+        this.handleChange = this.handleChange.bind(this);
+
+        this.onChange = this.onChange.bind(this);
+    }
+
     public render() {
-        return (
+        return [
             <limel-grid>
                 <grid-header>
-                    <h1>Heading</h1>
-                    <select class="dropdown">
-                        <option value="default"> Filter </option>
-                        <option value="filter 1"> Filter 1</option>
-                        <option value="filter 2"> Filter 2</option>
-                    </select>
-                    <div id="week-display">
-                        <p>Vecka x</p>
+                    <limel-icon badge={true} name="megaphone" size="medium"/>
+                    <h1>Sprint planner</h1>
+                    <div id="filter">
+                         <limel-select
+                          label="Limetype"
+                          value={this.selectValue}
+                          options={this.options}
+                          onChange={this.onChange}
+                          // multiple={true}
+                         />
+                        <p>Value: {JSON.stringify(this.selectValue)}</p>
                     </div>
+                    <div id="week-display">
+                    <p>
+                    <limel-date-picker
+                        type="week"
+                        label="week"
+                        value={this.dateValue}
+                        onChange={this.handleChange}
+                    />
+                    <p style={{ 'font-size': 'small' }}>
+                        Value:{' '}
+                        <code>
+                            {this.dateValue
+                                ? this.dateValue.toString()
+                                : JSON.stringify(this.dateValue)}
+                        </code>
+                    </p>
+                </p>
+ 
+                </div>
                 </grid-header>
                 <urgent-component>
                     <p>AKUT AKUT !!!</p>
@@ -108,6 +149,13 @@ export class Framework implements LimeWebComponent {
                 </grid-main>
 
             </limel-grid>    
-                    );
+        ];
+    }
+    private handleChange(event) {
+        this.dateValue = event.detail;
+    }
+
+    private onChange(event) {
+        this.selectValue = event.detail;
     }
 }
