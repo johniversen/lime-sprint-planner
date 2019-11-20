03935363409b3a034cc723bdf85bfa3c1ce4b554
hanna <fellwinghanna@gmail.com>
2019-11-20 14:35:13 +0100

    removed priorityValue & postId from the dialog. And changed dialog html to limel-list

--- limepkg-uni/frontend/src/components/lwc-limepkg-uni-framework/lwc-limepkg-uni-framework.tsx
+++ limepkg-uni/frontend/src/components/lwc-limepkg-uni-framework/lwc-limepkg-uni-framework.tsx
@@ -7,7 +7,8 @@ import {
 
 } from '@limetech/lime-web-components-interfaces';
 import { Component, Element, h, Prop, State } from '@stencil/core';
-import { Option, /*DialogHeading */ } from '@limetech/lime-elements';
+import { Option, ListItem } from '@limetech/lime-elements';
+
 
 @Component({
     tag: 'lwc-limepkg-uni-framework',
@@ -40,10 +41,11 @@ export class Framework implements LimeWebComponent {
         postId: number
     }];
 
+
     @State()
     private dialogIsOpen = false;
 
-    private dialogData: { title: string };
+    private dialogData: { title: string, priorityValue: number, postId: number};
 
     private http: HttpService;
 
@@ -62,7 +64,7 @@ export class Framework implements LimeWebComponent {
 
     public componentWillLoad() {
         this.http = this.platform.get(PlatformServiceName.Http);
-        console.log("componentWillLoad");
+        //console.log("componentWillLoad");
         //this.getDataFromEndPoint("solutionimprovement")
         this.getLimeTypes();
     }
@@ -75,19 +77,19 @@ export class Framework implements LimeWebComponent {
 
     private getDataFromEndPoint(limeType) {
         this.fetchingDataComplete = false;
-        console.log("getDataFromEndpoint() with limeType: " + limeType);
+        //console.log("getDataFromEndpoint() with limeType: " + limeType);
 
         this.http.get(`https://localhost/lime/limepkg-uni/test/?limetype=` + limeType).then(res => {
 
         //Detta funkar inte! Varför körs inte Rende(), trots state updateras?
             if (res.objects[0] == null) {
-                console.log("res = undefined");
+                //console.log("res = undefined");
                 this.fetchingDataComplete = false;
                 this.mainData = null;
-                console.log(this.mainData);
+                //console.log(this.mainData);
             }
             else {
-                console.log("Kommer vi hit?");
+                //console.log("Kommer vi hit?");
                 this.fetchingDataComplete = true;
                 this.updateData(res);
             }
@@ -111,36 +113,47 @@ export class Framework implements LimeWebComponent {
         this.dialogIsOpen = true;
         let item = this.mainData.find(obj => obj.postId === event.detail.value);
 
-        console.log("item");
-        console.log(item);
         this.dialogData = Object.assign({}, item);
 
-        let dialogOutput = [];
-        dialogOutput.push(<h1>{this.dialogData.title}</h1>);
+        let dialogOutput: Array<ListItem<any>> = [];
+       // dialogOutput.push(<h1>{this.dialogData.title}</h1>);
+       let title = <h1>{this.dialogData.title}</h1>;
         delete this.dialogData.title;
+        delete this.dialogData.priorityValue;
+        delete this.dialogData.postId;
 
         const entries = Object.entries(this.dialogData);
-        for (const [key, count] of entries) {
-            dialogOutput.push(<p><strong>{key}</strong>: {count}<hr></hr></p>);
+        
+        for (const [key, value] of entries) {
+            let item = {
+                text: key,
+                secondaryText: value
+            }
+            dialogOutput.push((item as ListItem)); 
         }
+
         this.dialog = <limel-dialog open={this.dialogIsOpen} onClose={this.closeDialog}>
-            <div>{dialogOutput}</div>
+            <div>
+                {title}
+                <limel-list items={dialogOutput}>
+                </limel-list>
+            </div>
             <limel-flex-container justify="end" slot="button">
                 <limel-button label="Ok" onClick={this.closeDialog} />
             </limel-flex-container>
         </limel-dialog>
-        console.log(this.mainData);
+        //console.log(this.mainData);
     }
 
     private closeDialog() {
-        console.log("Close dialog");
+        //console.log("Close dialog");
         this.dialogIsOpen = false;
         this.dialog = null;
     }
 
     public render() {
-        console.log("framework Render()");
-        console.log(this.mainData);
+        //console.log("framework Render()");
+        //console.log(this.mainData);
 
         //Måste ha nån flagga som kollar om hämtning av data är klar. Render() körs "för tidigt".
         let cardData = <h1>There are no data posts in the database</h1>;
@@ -182,9 +195,9 @@ export class Framework implements LimeWebComponent {
                         </p>
                     </div>
                 </grid-header>
-                <urgent-component>
-
-                </urgent-component>
+                <div id="urgent">
+                    
+                </div>
                 <grid-main>
                     {cardData}
                 </grid-main>
@@ -198,7 +211,7 @@ export class Framework implements LimeWebComponent {
 
     //Varför körs denna två gånger?
     private onChange(event) {
-        console.log("OnChange()");
+        //console.log("OnChange()");
         this.selectValue = event.detail;
         let limeType = event.detail.value;
         this.getDataFromEndPoint(limeType);
