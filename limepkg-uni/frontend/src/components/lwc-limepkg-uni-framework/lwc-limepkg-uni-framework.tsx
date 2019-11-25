a0c73bb736edd683a0167ca7049667470d063c4b
John Iversen <35525584+johniversen@users.noreply.github.com>
2019-11-25 10:27:44 +0100

    Revert "dialog filter null values - not done"

--- limepkg-uni/frontend/src/components/lwc-limepkg-uni-framework/lwc-limepkg-uni-framework.tsx
+++ limepkg-uni/frontend/src/components/lwc-limepkg-uni-framework/lwc-limepkg-uni-framework.tsx
@@ -125,19 +125,10 @@ export class Framework implements LimeWebComponent {
         const entries = Object.entries(this.dialogData);
         
         for (const [key, value] of entries) {
-            let item = {} 
-                if (value == "") {
-                    item = {
-                        text: key,
-                        secondaryText: "Not assigned"
-                 }
-                } else  {
-                    item = {
-                        text: key,
-                        secondaryText: value
-                 };
+            let item = {
+                text: key,
+                secondaryText: value
             }
-
             dialogOutput.push((item as ListItem)); 
         }
 
